65c83ab08b7d50ee127364b67008fc9cb303f95e
Vaibhav2482 <vaibhavnawale2482@gmail.com>
2026-01-21 12:12:24 +0530

    frontend routs setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,27 @@
-import Login from "./pages/login";
+import React from 'react'
+import { Routes, Route } from "react-router-dom";
+import Home from './pages/students/Home';
+import CourseList from './pages/students/CourseList';
+import CourseDetails from './pages/students/CourseDetails';
+import Player from'./pages/students/Player';
+import MyEnrollments from './pages/students/MyEnrollments';
+import Loading from './components/students/Loading';
 
-function App() {
+const App = () => {
   return (
-    <>
-      <Login />
-    </>
-  );
+    <div>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route  path='/course-list' element={<CourseList/>}/>
+        <Route  path='/course-list:input' element={<CourseList/>}/>
+        <Route  path='/course/:id' element={<CourseDetails/>}/>
+        <Route  path='/my-enrollments' element={<MyEnrollments/>}/>
+        <Route  path='/player/:courseId' element={<Player/>}/>
+        <Route  path='/loading/:path' element={<Loading />}/>
+      </Routes>
+    </div>
+  )
 }
 
-export default App;
+export default App
+
---
+++ client/src/pages/host/Mycourses.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Mycourses = () => {
+  return (
+    <div>
+      <h1>Mycourses</h1>
+    </div>
+  )
+}
+
+export default Mycourses
